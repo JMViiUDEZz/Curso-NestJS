2c1e3035c2b2ea63a9585e35e38d77f2c9343600
José María Viúdez <jmviudezp2003@gmail.com>
2022-09-28 20:01:10 +0200

    15

--- 01-TypeScript-intro/src/bases/03-classes.ts
+++ 01-TypeScript-intro/src/bases/03-classes.ts
@@ -1,10 +1,21 @@
 export class Pokemon {
 
+    get imageUrl() {
+        return `http://pokemon.com/${ this.id }.jpg`;
+    }
+
     constructor( 
         public readonly id: number,
-        public name: string
-    ) {
+        public name: string,
+        // public imageUrl: string,
+    ) {}
 
+    scream() {
+        console.log(`${ this.name.toUpperCase() }!!!`);
+    }
+
+    speak() {
+        console.log(`${ this.name }, ${ this.name }`);
     }
 }
 
@@ -12,3 +23,8 @@ export const charmander = new Pokemon( 4, 'Charmander' )
 
 // charmander.id = 10;
 // charmander.name = 'Mew';
+
+console.log( charmander )
+
+charmander.scream();
+charmander.speak();

cc3b2351e3c66329e4e99ace2ccd5980a555ae20
José María Viúdez <jmviudezp2003@gmail.com>
2022-09-28 13:57:26 +0200

    12

---
+++ 01-TypeScript-intro/src/bases/02-objects.ts
@@ -0,0 +1,23 @@
+export const pokemonIds = [1,20,30,40];
+
+// pokemonIds.push( +'1' );
+
+    interface Pokemon {
+        id: number;
+        name: string;
+        age?: number | undefined;
+    }
+
+   export const bulbasaur:Pokemon = {
+        id: 1,
+        name: 'bulbasaur',
+        age: 2
+    }
+    
+   export const charmander:Pokemon = {
+        id: 4,
+        name: 'charmander',
+        age: 1
+}
+
+    console.log(bulbasaur);
--- 01-TypeScript-intro/src/main.ts
+++ 01-TypeScript-intro/src/main.ts
@@ -1,9 +1,13 @@
 import './style.css'
+
 import typescriptLogo from './typescript.svg'
 import { setupCounter } from './counter'
 
+import { pokemonIds } from './bases/02-objects'
+import { bulbasaur } from './bases/02-objects'
+
 //importamos las variables exportadas 
-import { name, age }  from './bases/01-types';
+// import { name, age }  from './bases/01-types';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -13,7 +17,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript + ${ name }, ${ age }!!</h1>
+    <h1>Vite + TypeScript + ${ pokemonIds.join(',')} + ${ bulbasaur.name }!!!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>

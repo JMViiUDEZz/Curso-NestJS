a79621943af94a2e6f8b6f937c09b2d384616632
José María Viúdez <jmviudezp2003@gmail.com>
2022-09-28 19:48:51 +0200

    14

--- 01-TypeScript-intro/src/main.ts
+++ 01-TypeScript-intro/src/main.ts
@@ -9,6 +9,8 @@ import { setupCounter } from './counter'
 //importamos las variables exportadas 
 // import { name, age }  from './bases/01-types';
 
+import { charmander } from './bases/03-classes';
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -17,7 +19,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript!</h1>
+    <h1>Vite + TypeScript + ${ charmander.name } + ${ charmander.id }!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>

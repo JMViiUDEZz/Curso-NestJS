f741ecc0c58c0869ed28688c07e56781293163d5
José María Viúdez <jmviudezp2003@gmail.com>
2022-09-28 12:16:27 +0200

    10

--- 01-TypeScript-intro/src/bases/01-types.ts
+++ 01-TypeScript-intro/src/bases/01-types.ts
@@ -1,4 +1,4 @@
 //Explicar  el DataBinding de variables entre el controlador (ts) y la vista (html)
 export const name = 'Jose';
-
 export const edad: number = 19;
+export const IsValid:boolean = true;

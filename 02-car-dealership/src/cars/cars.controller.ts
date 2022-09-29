5e525602c07cc42c7b74e73b017420f5f7be6f66
José María Viúdez <jmviudezp2003@gmail.com>
2022-09-29 17:19:40 +0200

    46

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 @Controller('cars')
 export class CarsController {
 
@@ -17,9 +18,9 @@ export class CarsController {
         return this.carsService.findOneById( id );
     }
 
-    @Post()
-    createCar( @Body() body:any ) {
-        return body
+    @Post() 
+    createCar( @Body() createCarDto: CreateCarDto ) {
+        return createCarDto;
     }
 
     @Patch(':id')

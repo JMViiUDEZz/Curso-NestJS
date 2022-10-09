f38e699a27d99f9203965f94b16a7d82bff1d85b
José María Viúdez <jmviudezp2003@gmail.com>
2022-10-09 21:00:40 +0200

    93

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -5,6 +5,7 @@ import { Model } from 'mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { async } from '@angular/core/testing';
+import { stringify } from 'querystring';
 
 @Injectable()
 export class SeedService {
@@ -19,17 +20,44 @@ export class SeedService {
   ) {}
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
 
-    data.results.forEach(async({ name, url }) => {
+    await this.pokemonModel.deleteMany(); //delete * from pokemons;
+
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    
+    //1ª FORMA (FACIL)
+    // const insertPromisesArray = [];
+
+    // data.results.forEach(({ name, url }) => {
+
+    //   const segments = url.split('/');
+    //   const no = +segments[ segments.length - 2 ];
+
+    //   // const pokemon = await this.pokemonModel.create({ name, no });
+    //   insertPromisesArray.push(
+    //     this.pokemonModel.create({ name, no })
+    //   )
+
+    //   // console.log({ name, no })
+    // });
+
+    // await Promise.all( insertPromisesArray );
+
+    //2ª FORMA (FACIL Y EFICIENTE)
+    const pokemonToInsert: { name: string, no: number}[] = [];
+
+    data.results.forEach(({ name, url }) => {
 
       const segments = url.split('/');
       const no = +segments[ segments.length - 2 ];
 
-      const pokemon = await this.pokemonModel.create({ name, no });
+      // const pokemon = await this.pokemonModel.create({ name, no });
+      pokemonToInsert.push({ name, no }); //[{name: bulbasaur, no: 1}]
 
       // console.log({ name, no })
-    })
+    });
+
+    await this.pokemonModel.insertMany( pokemonToInsert );
     
     return 'Seed Executed';
   }

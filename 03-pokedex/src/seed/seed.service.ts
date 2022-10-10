856bacdf7cebafa70ccb7abb372e67e879b28be5
José María Viúdez <jmviudezp2003@gmail.com>
2022-10-10 18:21:23 +0200

    94

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
+
+// import axios, { AxiosInstance } from 'axios';
+
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
-import { async } from '@angular/core/testing';
-import { stringify } from 'querystring';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
+
+// import { async } from '@angular/core/testing';
+// import { stringify } from 'querystring';
 
 @Injectable()
 export class SeedService {
-
-  private readonly axios: AxiosInstance = axios;
   
   constructor(
     
     @InjectModel( Pokemon.name )
     private readonly pokemonModel: Model<Pokemon>,
+    
+    private readonly http: AxiosAdapter,
 
   ) {}
 
@@ -23,7 +27,7 @@ export class SeedService {
 
     await this.pokemonModel.deleteMany(); //delete * from pokemons;
 
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
     
     //1ª FORMA (FACIL)
     // const insertPromisesArray = [];

d879417bfdcadc870e5693fa679aa98ba7e2e65b
José María Viúdez <jmviudezp2003@gmail.com>
2022-10-13 17:12:49 +0200

    124

--- 04-teslo-shop/src/products/products.service.ts
+++ 04-teslo-shop/src/products/products.service.ts
@@ -1,26 +1,113 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+// import { PaginationDto } from 'src/common/dtos/pagination.dto';
+
+import { Product } from './entities/product.entity';
+// import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(
+
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    
+    try {
+
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save( product );
+
+      return product;
+      
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException('Ayuda!')
+      // this.handleDBExceptions(error);
+    }
+
+
   }
 
-  findAll() {
-    return `This action returns all products`;
-  }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
-  }
+  // findAll( paginationDto: PaginationDto ) {
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
+  //   const { limit = 10, offset = 0 } = paginationDto;
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
-  }
+  //   return this.productRepository.find({
+  //     take: limit,
+  //     skip: offset,
+  //     // TODO: relaciones
+  //   })
+  // }
+
+  // async findOne( term: string ) {
+
+  //   let product: Product;
+
+  //   if ( isUUID(term) ) {
+  //     product = await this.productRepository.findOneBy({ id: term });
+  //   } else {
+  //     const queryBuilder = this.productRepository.createQueryBuilder(); 
+  //     product = await queryBuilder
+  //       .where('UPPER(title) =:title or slug =:slug', {
+  //         title: term.toUpperCase(),
+  //         slug: term.toLowerCase(),
+  //       }).getOne();
+  //   }
+
+
+  //   if ( !product ) 
+  //     throw new NotFoundException(`Product with ${ term } not found`);
+
+  //   return product;
+  // }
+
+  // async update( id: string, updateProductDto: UpdateProductDto ) {
+
+  //   const product = await this.productRepository.preload({
+  //     id: id,
+  //     ...updateProductDto
+  //   });
+
+  //   if ( !product ) throw new NotFoundException(`Product with id: ${ id } not found`);
+
+  //   try {
+  //     await this.productRepository.save( product );
+  //     return product;
+      
+  //   } catch (error) {
+  //     this.handleDBExceptions(error);
+  //   }
+
+  // }
+
+  // async remove(id: string) {
+  //   const product = await this.findOne( id );
+  //   await this.productRepository.remove( product );
+    
+  // }
+
+
+  // private handleDBExceptions( error: any ) {
+
+  //   if ( error.code === '23505' )
+  //     throw new BadRequestException(error.detail);
+    
+  //   this.logger.error(error)
+  //   // console.log(error)
+  //   throw new InternalServerErrorException('Unexpected error, check server logs');
+
+  // }
+
 }

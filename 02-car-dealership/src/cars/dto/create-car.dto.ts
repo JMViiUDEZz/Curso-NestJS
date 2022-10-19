c5f99a1825f1677e61bf7115f0e93e3fdb7caa05
José María Viúdez <jmviudezp2003@gmail.com>
2022-10-19 14:01:20 +0200

    actualizacion proyecto 02 comentado create-car.dto.ts

--- 02-car-dealership/src/cars/dto/create-car.dto.ts
+++ 02-car-dealership/src/cars/dto/create-car.dto.ts
@@ -1,11 +1,13 @@
 import { IsString } from "class-validator";
 
+//Dto: En esencia, es como una interfaz(estructura de los datos) que, a su vez, valida los datos que vienen de la request --> Por ello, sustituiremos la Interfaz por el Dto (podremos eliminar la interfaz de nuestro proyecto, ya que en su lugar declararemos el Dto)
 export class CreateCarDto {
     //todos los campos obligatorios a la hora de insertar
     @IsString() //validador
     readonly brand: string; //todos suelen ser readonly, para que salga un error si accidentalmente hacemos alguna modificacion
 
     @IsString()
-    readonly model: string;
+    readonly model: string; //readonly: impide la modificacion de los datos del parametro al pasar por la api(controlador y servicio) --> Por tanto, solamente valida la data (solo lectura)
+    id: number;
     
 }

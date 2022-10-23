95f056701b7ab733ec07b1800211c7949435c154
José María Viúdez <jmviudezp2003@gmail.com>
2022-10-23 21:58:19 +0200

    148

--- 04-teslo-shop/src/seed/seed.service.ts
+++ 04-teslo-shop/src/seed/seed.service.ts
@@ -21,18 +21,18 @@ export class SeedService {
   private async insertNewProducts() {
     await this.productsService.deleteAllProducts(); //elimina toda la data de la BDD
 
-  //   const products = initialData.products;
+    const products = initialData.products; //tomamos toda la data en products
 
-  //   const insertPromises = [];
+    const insertPromises = [];
 
-  //   products.forEach( product => {
-  //     insertPromises.push( this.productsService.create( product ) );
-  //   });
+    products.forEach( product => { //con cada uno de estos productos
+      insertPromises.push( this.productsService.create( product ) ); //espera a que cada una de estas promesas se resuelvan
+    });
 
-  //   await Promise.all( insertPromises );
+    await Promise.all( insertPromises ); //ejecuta el array con cada uno de los valores que cada una de estas promesas resuelva
 
 
-  //   return true;
+    return true;
   }
 
 
